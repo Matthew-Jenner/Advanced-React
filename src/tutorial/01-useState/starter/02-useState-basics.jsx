968a57f73000d4beb829768f3b9808300934387d
Matthew-Jenner <121139184+Matthew-Jenner@users.noreply.github.com>
2024-01-30 12:12:32 +0000

    use state basics

--- src/tutorial/01-useState/starter/02-useState-basics.jsx
+++ src/tutorial/01-useState/starter/02-useState-basics.jsx
@@ -1,5 +1,18 @@
+import {useState} from "react"
+
+
 const UseStateBasics = () => {
-  return <h2>useState basics</h2>;
+  // const value = (useState('hello')[0]);
+  // console.log(value);
+
+  const [count, setCount] = useState(0)
+  const handleClick = () => {
+    setCount(count + 1)
+  }
+  return <div>
+    <h4> You Clicked {count} times</h4>
+    <button type="vutton" className="btn" onClick={handleClick}> Click Me</button>
+  </div>
 };
 
 export default UseStateBasics;
