e75e2be195ebadfe575b626c02b580dcc1b58767
Matthew-Jenner <121139184+Matthew-Jenner@users.noreply.github.com>
2024-01-30 13:17:38 +0000

    useState array manipulation

--- src/tutorial/01-useState/starter/03-useState-array.jsx
+++ src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -1,19 +1,40 @@
-import {data} from '../../../data'
-import React from 'react';
+import { data } from "../../../data";
+import React from "react";
 
 const UseStateArray = () => {
-  const [people, setPeople]= React.useState(data)
-  return (
-    <div>
-      {people.map((person) => {
-        const {id, name} = person
-      // console.log(person)
-      return <div key={id}>
-        <h4>{name}</h4>
-        </div>
-})}
-    </div>
-  )
+	const [people, setPeople] = React.useState(data);
+
+	const removeItem = (id) => {
+    console.log(id);
+    const newPeople = people.filter((person) => person.id !== id)
+    setPeople(newPeople)
+  };
+	const clearAllItems = () => {
+		setPeople([]);
+	};
+
+	return (
+		<div>
+			{people.map((person) => {
+				const { id, name } = person;
+				// console.log(person)
+				return (
+					<div key={id}>
+						<h4>{name}</h4>
+						<button type="button" onClick={() => removeItem(id)
+            }> Remove </button>
+					</div>
+				);
+			})}
+			<button
+				type="button"
+				style={{ marginTop: "2rem" }}
+				className="btn"
+				onClick={clearAllItems}>
+				Clear Items
+			</button>
+		</div>
+	);
 };
 
 export default UseStateArray;
